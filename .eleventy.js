a9bd43d963616e31b42b9676e3c49e22aa470883
Annabeth Kisling <hasederhi.12@gmail.com>
2026-04-02 20:32:22 +0200

    fix path prefix;

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,6 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
-    pathPrefix: "/klavier-spielen-lernen.at//",
     dir: {
       input: "src",
       includes: "_includes",
